af288b781fd03bc98b95dd2e240b55b1ac3e16af
iagoborna <iagooliveiraleite@gmail.com>
2026-04-05 01:06:37 -0300

    Add files via upload

---
+++ rolamentos-data.js
@@ -0,0 +1,58 @@
+window.rolamentosDB = [
+  // RÍGIDOS DE ESFERAS - SÉRIE 6000
+  { codigo: "6000", tipo: "rigido_esferas", d: 10, D: 26, B: 8 },
+  { codigo: "6001", tipo: "rigido_esferas", d: 12, D: 28, B: 8 },
+  { codigo: "6002", tipo: "rigido_esferas", d: 15, D: 32, B: 9 },
+  { codigo: "6003", tipo: "rigido_esferas", d: 17, D: 35, B: 10 },
+  { codigo: "6004", tipo: "rigido_esferas", d: 20, D: 42, B: 12 },
+  { codigo: "6005", tipo: "rigido_esferas", d: 25, D: 47, B: 12 },
+  { codigo: "6006", tipo: "rigido_esferas", d: 30, D: 55, B: 13 },
+  { codigo: "6007", tipo: "rigido_esferas", d: 35, D: 62, B: 14 },
+  { codigo: "6008", tipo: "rigido_esferas", d: 40, D: 68, B: 15 },
+  { codigo: "6009", tipo: "rigido_esferas", d: 45, D: 75, B: 16 },
+  { codigo: "6010", tipo: "rigido_esferas", d: 50, D: 80, B: 16 },
+
+  // RÍGIDOS DE ESFERAS - SÉRIE 6200
+  { codigo: "6200", tipo: "rigido_esferas", d: 10, D: 30, B: 9 },
+  { codigo: "6201", tipo: "rigido_esferas", d: 12, D: 32, B: 10 },
+  { codigo: "6202", tipo: "rigido_esferas", d: 15, D: 35, B: 11 },
+  { codigo: "6203", tipo: "rigido_esferas", d: 17, D: 40, B: 12 },
+  { codigo: "6204", tipo: "rigido_esferas", d: 20, D: 47, B: 14 },
+  { codigo: "6205", tipo: "rigido_esferas", d: 25, D: 52, B: 15 },
+  { codigo: "6206", tipo: "rigido_esferas", d: 30, D: 62, B: 16 },
+  { codigo: "6207", tipo: "rigido_esferas", d: 35, D: 72, B: 17 },
+  { codigo: "6208", tipo: "rigido_esferas", d: 40, D: 80, B: 18 },
+  { codigo: "6209", tipo: "rigido_esferas", d: 45, D: 85, B: 19 },
+  { codigo: "6210", tipo: "rigido_esferas", d: 50, D: 90, B: 20 },
+
+  // RÍGIDOS DE ESFERAS - SÉRIE 6300
+  { codigo: "6300", tipo: "rigido_esferas", d: 10, D: 35, B: 11 },
+  { codigo: "6301", tipo: "rigido_esferas", d: 12, D: 37, B: 12 },
+  { codigo: "6302", tipo: "rigido_esferas", d: 15, D: 42, B: 13 },
+  { codigo: "6303", tipo: "rigido_esferas", d: 17, D: 47, B: 14 },
+  { codigo: "6304", tipo: "rigido_esferas", d: 20, D: 52, B: 15 },
+  { codigo: "6305", tipo: "rigido_esferas", d: 25, D: 62, B: 17 },
+  { codigo: "6306", tipo: "rigido_esferas", d: 30, D: 72, B: 19 },
+  { codigo: "6307", tipo: "rigido_esferas", d: 35, D: 80, B: 21 },
+  { codigo: "6308", tipo: "rigido_esferas", d: 40, D: 90, B: 23 },
+  { codigo: "6309", tipo: "rigido_esferas", d: 45, D: 100, B: 25 },
+  { codigo: "6310", tipo: "rigido_esferas", d: 50, D: 110, B: 27 },
+
+  // AUTOCOMPENSADORES DE ESFERAS - SÉRIE 1200
+  { codigo: "1204", tipo: "autocompensador_esferas", d: 20, D: 47, B: 14 },
+  { codigo: "1205", tipo: "autocompensador_esferas", d: 25, D: 52, B: 15 },
+  { codigo: "1206", tipo: "autocompensador_esferas", d: 30, D: 62, B: 16 },
+  { codigo: "1207", tipo: "autocompensador_esferas", d: 35, D: 72, B: 17 },
+  { codigo: "1208", tipo: "autocompensador_esferas", d: 40, D: 80, B: 18 },
+
+  // BASE PRONTA PARA EXPANSÃO
+  // AUTOCOMPENSADOR DE ROLOS
+  // Exemplo de estrutura:
+  // { codigo: "22205", tipo: "autocompensador_rolos", d: 25, D: 52, B: 18 },
+
+  // ROLOS CILÍNDRICOS
+  // { codigo: "NU205", tipo: "rolos_cilindricos", d: 25, D: 52, B: 15 },
+
+  // CONTATO ANGULAR
+  // { codigo: "7205", tipo: "contato_angular", d: 25, D: 52, B: 15 }
+];
